7b0b3d0c60fed3d5576a8b6f654e2dd5df8c87ba
Ragai Ahmed <ragaiawad@gmail.com>
2021-05-21 11:21:26 +0200

    Add torture channel

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
 import discord
 import os
+import random
 
 token = os.getenv("DiscordBot")
 client = discord.Client()
+
+Ta3zeeb = ["la1.mp3", "la2.mp3","la3.mp3","la4.mp3"]
+
 
 @client.event
 async def on_ready():
@@ -18,5 +22,38 @@ async def on_message(message):
 
     if message.content.startswith('!Catch '):
         await message.channel.send('Woah you ain\'t my boss !!')
+
+
+local_random = random.Random()
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    # Check if a desired channel
+    if after.channel and after.channel.name == "الامن الوطني" and member.id != client.user.id:
+        is_connected = any((i.id == client.user.id) for i in after.channel.members)
+
+        # Connect if not connected to the user channel
+        if not is_connected:
+            voice = discord.utils.get(client.voice_clients, guild=member.guild)
+
+            # If connected to another voice then disconnect
+            if voice is not None:
+                await voice.disconnect()
+
+            await after.channel.connect()
+            print("Entered voice xDD")
+
+        # Get voice client object
+        voice = discord.utils.get(client.voice_clients, guild=member.guild)
+
+        # Check if not playing
+        if not voice.is_playing():
+            voice.play(discord.FFmpegPCMAudio(local_random.choice(Ta3zeeb)))  # Play random ta3zeeb
+            print("Playing torture sounds :3")
+
+        # Unmute self
+        for m in after.channel.members:
+            if m.id == client.user.id:
+                await m.edit(mute=False)
 
 client.run(token)
