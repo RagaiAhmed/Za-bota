5100558b12c8a675bb1ffab5400aa9e9a7aa2e20
Ragai Ahmed <ragaiawad@gmail.com>
2021-05-24 22:32:31 +0200

    Support commands from admin Added !come command

--- bot.py
+++ bot.py
@@ -16,13 +16,30 @@ async def on_ready():
 @client.event
 async def on_message(message):
     print("message received")
+    isBoss = discord.utils.get(message.author.roles, name="admin")
 
-    if message.content.startswith('!Hello'):
-        await message.channel.send('Hello! I am Za-Bota, will be under the command of the Minister Of Interior :D')
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello! I am Za-Bota, will be under the command of \'admins\'')
         await message.channel.send('I have automated drones, Wanna see some "drone" parts in my office ? ;)')
 
-    if message.content.startswith('!Catch '):
-        await message.channel.send('Woah you ain\'t my boss !!')
+    if message.content.startswith('!catch'):
+        if isBoss:
+            await message.channel.send('Woah trying my best boss, I can also offer other services if you wish ;)')
+        else:
+            await message.channel.send('Woah you ain\'t my boss !!')
+
+    if message.content.startswith('!detain'):
+        if isBoss:
+            await message.channel.send('Woah trying my best boss, I can also offer other services if you wish ;)')
+        else:
+            await message.channel.send('Woah you ain\'t my boss !!')
+
+    if message.content.startswith('!come'):
+        if isBoss:
+            await message.channel.send('I am coming.. UwU')
+            await torture(message.author, message.author.voice)
+        else:
+            await message.channel.send('Woah you ain\'t my boss !!')
 
 
 local_random = random.Random()
@@ -32,31 +49,35 @@ local_random = random.Random()
 async def on_voice_state_update(member, before, after):
     # Check if a desired channel
     if after.channel and after.channel.name == "الامن الوطني" and member.id != client.user.id:
-        is_connected = any((i.id == client.user.id) for i in after.channel.members)
+        await torture(member, after)
 
-        # Connect if not connected to the user channel
-        if not is_connected:
-            voice = discord.utils.get(client.voice_clients, guild=member.guild)
 
-            # If connected to another voice then disconnect
-            if voice is not None:
-                await voice.disconnect()
+async def torture(member, after):
+    is_connected = any((i.id == client.user.id) for i in after.channel.members)
 
-            await after.channel.connect()
-            print("Entered voice xDD")
+    # Connect if not connected to the user channel
+    if not is_connected:
+        voice = discord.utils.get(client.voice_clients, guild=member.guild)
+
+        # If connected to another voice then disconnect
+        if voice is not None:
+            await voice.disconnect()
+
+        await after.channel.connect()
+        print("Entered voice xDD")
 
     # Get voice client object
     voice = discord.utils.get(client.voice_clients, guild=member.guild)
 
-        # Check if not playing
-        if not voice.is_playing():
-            voice.play(discord.FFmpegPCMAudio(local_random.choice(Ta3zeeb)))  # Play random ta3zeeb
-            print("Playing torture sounds :3")
+    # Check if not playing
+    if not voice.is_playing():
+        voice.play(discord.FFmpegPCMAudio(local_random.choice(Ta3zeeb)))  # Play random ta3zeeb
+        print("Playing torture sounds :3")
 
-        # Unmute self
-        for m in after.channel.members:
-            if m.id == client.user.id:
-                await m.edit(mute=False)
+    # Unmute self
+    for m in after.channel.members:
+        if m.id == client.user.id:
+            await m.edit(mute=False)
 
 
 client.run(token)
